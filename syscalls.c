ff48e72320d56e17679fa0b52449dcc07b3aa6a8
Genny <gen@nyble.dev>
2021-08-22 14:41:18 -0500

    Add explanation for syscalls

--- syscalls.c
+++ syscalls.c
@@ -1,2 +1,6 @@
+// These are needed to make newlib happy enough to compile. If you want printf
+// and similar to go to a UART then you'd have to change these. Right now, I
+// don't need that so they are just cute little stubs.
+
 int _read(int file, char *ptr, int len) { return -1; }
 int _write(int file, char *ptr, int len) { return -1; }
